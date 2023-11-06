7874584ffb734eddfb9ee90a8a174963ff01e5f7
Abir Zbidi <abir.zbidi.visioad@gmail.com>
2023-11-06 14:47:41 +0200

    My commit message

---
+++ components/CustomButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { CustomButtonProps } from "@/types";
+
+import Image from "next/image";
+
+
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, isDisabled, rightIcon }: CustomButtonProps) => {
+    return (
+        <button disabled={false} type={"button" || btnType} className={`custom-btn ${containerStyles}`} onClick={handleClick} >
+
+            <span className="flex-1 ${textStyles} "> {title}</span>
+            {rightIcon && (
+                <div className="relative w-6 h-5">
+                    <Image src={rightIcon} alt="right icon" 
+                    fill
+                    className="object-contain"/>
+                </div>
+            )}
+        </button>)
+}
+
+export default CustomButton
